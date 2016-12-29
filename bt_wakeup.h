7248ae097f95e3307e899817ce4da02b92343b56
yuelei0714 <yuelei0714@thundersoft.com>
2016-12-29 20:38:27 +0800

    V1.0

---
+++ bt_wakeup.h
@@ -0,0 +1,6 @@
+#ifndef __BT_WAKEUP_H__
+#define __BT_WAKEUP_H__
+
+void bcm_wakeup(void);
+
+#endif
---
+++ power_on.c
@@ -0,0 +1,69 @@
+#include <linux/module.h>
+#include <linux/input.h>
+#include <linux/bitops.h>
+#include <linux/export.h>
+#include "bt_wakeup.h"
+
+static struct input_dev *button_dev;  
+
+void bcm_wakeup(void)  
+{  
+	printk(KERN_ERR"--yuelei--1>%s:Enter!\n" , __func__);  
+	input_report_key(button_dev, KEY_WAKEUP, 1);  
+	input_sync(button_dev);  
+}
+
+static int __init button_init(void)  
+{  
+	int error;  
+	printk(KERN_ERR"--yuelei--1>%s:Enter!\n" , __func__);  
+
+	//	if (request_irq(BUTTON_IRQ, button_interrupt, 0, "button",NULL)) {  
+	//		printk(KERN_ERR"button.c: Can't allocate irq %d\n", button_irq);  
+	//		return -EBUSY;  
+	//	}        
+
+	button_dev = input_allocate_device();  
+	if (!button_dev) {  
+		printk(KERN_ERR"button.c: Not enough memory\n");  
+		error = -ENOMEM;  
+		goto err_free_irq;  
+	}  
+
+	//button_dev->evbit[0] = BIT(EV_KEY);  
+	//button_dev->keybit[LONG(KEY_POWER)] = BIT(KEY_POWER);  
+
+	button_dev->evbit[0] = BIT_MASK(EV_KEY);                                                                                                                                
+	button_dev->keybit[BIT_WORD(KEY_WAKEUP)] = BIT_MASK(KEY_WAKEUP);
+
+	error = input_register_device(button_dev);  
+	if (error) {  
+		printk(KERN_ERR"button.c: Failed to register device\n");  
+		goto err_free_dev;  
+	}  
+
+	//input_report_key(button_dev, KEY_WAKEUP , 1);  
+	//input_sync(button_dev);  
+	printk(KERN_ERR"--yuelei--2>%s:Out!\n" , __func__);  
+
+	return 0;  
+
+err_free_dev:  
+	input_free_device(button_dev);  
+err_free_irq:  
+	//free_irq(BUTTON_IRQ, button_interrupt);  
+	printk(KERN_ERR"--yuelei--3>%s:Out_error!\n" , __func__);  
+	return error;  
+}  
+
+static void __exit button_exit(void)  
+{  
+	printk(KERN_ERR"--yuelei--1>%s:Enter!\n" , __func__);  
+	input_unregister_device(button_dev);  
+	//free_irq(BUTTON_IRQ, button_interrupt);  
+}  
+
+module_init(button_init);  
+module_exit(button_exit);   
+
+MODULE_LICENSE("GPL");
